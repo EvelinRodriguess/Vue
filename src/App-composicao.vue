377723463dc8d22a36563ea7ac1244adf6f9c40e
EvelinRodriguess <evelinrodrigues.ifc@gmail.com>
2023-03-10 16:54:29 -0300

    .

--- src/App-composicao.vue
+++ src/App-composicao.vue
@@ -1,36 +1,41 @@
-<script setup>
-import{ ref } from 'vue'
-const contador = ref(10)
-
-function incrementar() {
-    contador.value++
-}
-function decrementar(){
-    if(contador.value > 0){
-        contador.value--
+<script>
+export default {
+  data(){
+    return{
+      contador: 10
     }
+  },
+  methods: {
+    incrementar(){
+      this.contador++
+    },
+    decrementar(){
+      if(this.contador > 0)
+      this.contador--
+    }
+  },
+  resetar(){
+    this.contador = 0
+  }
 }
-function resetar(){
-    contador.value = 0
-}
+
 </script>
 
 <template>
-    <button @click="incrementar">+</button>
-    <button @click="decrementar">-</button>
-    <button @click="resetar">C</button>
-    <p>Valor do contador: {{ contador }}</p>
-  </template>
-  
-  <style scoped>
-  button {
-    margin-left: 10px;
-    background-color: rgb(22, 86, 214);
-    color: aliceblue;
-    border: none;
-    border-radius: 5px;
-    padding: 5px 10px;
-    cursor: pointer;
-  }
-  </style>
-  
+  <button @click="incrementar">+</button>
+  <button @click="decrementar">-</button>
+  <button @click="resetar">C</button>
+  <p>Valor do contador: {{ contador }}</p>
+</template>
+
+<style scoped>
+button {
+  margin-left: 10px;
+  background-color: rgb(22, 86, 214);
+  color: aliceblue;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+</style>
